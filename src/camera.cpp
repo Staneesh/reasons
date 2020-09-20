151d6c4f1349a2ec327225a265cecbf0bc6a2fc2
Staneesh <blkreasons@outlook.com>
2020-09-20 16:09:01 +0200

    🏗 Updated makefile and progress on camera

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,2 +1,18 @@
 #include "camera.hpp"
 
+void Camera::update_vectors()
+{
+    vec3 new_front;
+    new_front.x = cos(radians(yaw)) * cos(radians(pitch));
+    new_front.y = sin(radians(pitch));
+    new_front.z = sin(radians(yaw)) * cos(radians(pitch));
+
+    front = normalize(new_front);
+    right = normalize(cross(front, world_up));
+    up = normalize(cross(right, front));
+}
+
+Camera::Camera(vec3 position, vec3 up, float yaw, float pitch)
+{
+
+}
--- src/camera.hpp
+++ src/camera.hpp
@@ -1,4 +1,41 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include "../include/glad/glad.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace glm;
+
+class Camera
+{
+    vec3 position;
+    vec3 front;
+    vec3 up;
+    vec3 right;
+    vec3 world_up;
+
+    float yaw, pitch;
+    float movement_speed;
+    float sensitivity;
+    float zoom;
+
+    static const float constexpr default_yaw = -90.0f;
+    static const float constexpr default_pitch = 0.0f;
+    static const float constexpr default_speed = 2.5f;
+    static const float constexpr default_sensitivity = 0.1f;
+    static const float constexpr default_zoom = 45.0f;
+    
+    void update_vectors();
+public:
+
+    Camera(
+        vec3 position = vec3(0.0f, 0.0f, 0.0f), 
+        vec3 up = vec3(0.0f, 1.0f, 0.0f), 
+        float yaw = default_yaw, 
+        float pitch = default_pitch);
+
+    
+};
+
 #endif

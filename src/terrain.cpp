2a9897c593f9eec6f00be93e7f963d255a14bda1
Staneesh <blkreasons@outlook.com>
2020-09-22 13:53:30 +0200

    ✨ Claculating vertex indices for ebo

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -5,6 +5,7 @@ void Terrain::generate(
     float mesh_size_pass, 
     unsigned number_of_tiles_per_side)
 {
+    number_of_triangles = number_of_tiles_per_side * number_of_tiles_per_side * 2;
     position = position_pass;
     mesh_size = mesh_size_pass;
     tile_size = mesh_size_pass / number_of_tiles_per_side;
@@ -24,8 +25,24 @@ void Terrain::generate(
         for (unsigned x = 0; x < number_of_tiles_per_side + 1; ++x)
         {
             vertex_positions.push_back(current_vertex_position);
-            std::cout<<current_vertex_position.x<<" "<<current_vertex_position.y<<" "<<current_vertex_position.z<<std::endl;
+            //std::cout<<current_vertex_position.x<<" "<<current_vertex_position.y<<" "<<current_vertex_position.z<<std::endl;
             current_vertex_position += dx;
+            
+            unsigned bottom_left = x + z * (number_of_tiles_per_side + 1);
+            unsigned bottom_right = bottom_left + 1;
+            unsigned top_left = bottom_left + number_of_tiles_per_side + 1;
+            unsigned top_right = top_left + 1;
+            triangle_indices.push_back(glm::uvec3(x + 1, x, number_of_tiles_per_side + 1 + x));
+            triangle_indices.push_back(glm::uvec3(x + 1, number_of_tiles_per_side + 1 + x, number_of_tiles_per_side + 2 + x));
         }
     }
+    
+#if 0
+    for (unsigned i = 0; i < triangle_indices.size(); ++i)
+    {
+        auto cur = triangle_indices[i];
+        std::cout<<cur.x<<' '<<cur.y<<' '<<cur.z<<std::endl;
+    }
+#endif 
+
 }
--- src/terrain.hpp
+++ src/terrain.hpp
@@ -2,14 +2,19 @@
 #define TERRAIN_H
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include <vector>
 #include <iostream>
 
 class Terrain
 {
     unsigned number_of_vertices;
+    unsigned number_of_triangles;
     std::vector<glm::vec3> vertex_positions;
-    unsigned vbo, vao;
+    std::vector<glm::uvec3 > triangle_indices; 
+
+    unsigned vbo, vao, ebo;
     float mesh_size;
     float tile_size;
     glm::vec3 position;
